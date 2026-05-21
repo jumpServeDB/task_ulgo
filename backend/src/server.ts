c42a320ce8eea91d7a53fd545220a68b1f7b0c09
jumpServeDB <dbalint88@proton.me>
2026-05-21 22:22:32 +0100

    Modify a few missed settings, lay out main server module.

---
+++ backend/src/server.ts
@@ -0,0 +1,40 @@
+import cors from "cors";
+import "dotenv/config";
+import type { NextFunction, Request, Response } from "express";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT ? Number(process.env.PORT) : 4000
+app.use(express.json());
+
+// log incoming requests
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(req.method, req.url);
+    if (req.body) {
+        console.log("body:", req.body);
+    } else {
+        console.log("No request body.");
+    }
+    next();
+})
+
+// routes
+
+// app.use("/api/user", userRoutes);
+// app.use("/api/tasks", taskRoutes)
+
+// connect to DB
+
+const mongoURI = process.env.MONGO_URI ?? "";
+mongoose
+.connect(mongoURI)
+.then(() => {
+    app.listen(PORT, () => {
+        console.log("connected to db, listening on port ", PORT);
+    });
+})
+.catch((error: unknown) => {
+    console.error("DB connection erro:", error);
+});
