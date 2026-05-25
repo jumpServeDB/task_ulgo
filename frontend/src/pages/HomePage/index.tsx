e07171afebfeb721fb37686741425b359406a4fa
jumpServeDB <dbalint88@proton.me>
2026-05-25 20:28:57 +0100

    Wire up createTask endpoint. Implement page refresh and clean up isLoading / display logic

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -14,6 +14,7 @@ export default function HomePage() {
   const sessionContext = useContext(SessionContext);
   const [isLoading, setIsLoading] = useState(true);
   const [tasks, setTasks] = useState<Task[] | null>(null);
+  const [inputValue, setInputValue] = useState("");
 
   const getTasks = useCallback(async () => {
     setIsLoading(true);
@@ -24,6 +25,15 @@ export default function HomePage() {
     console.log(tasks);
     setIsLoading(false);
   }, []);
+
+  const addTask = async (title: string) => {
+    setTasks(null);
+    setIsLoading(true);
+    const response = await taskServices.addTask(title);
+    const data = await response.json();
+    console.log(data);
+    getTasks();
+  };
 
   useEffect(() => {
     const fetchTasks = async () => {
@@ -47,21 +57,29 @@ export default function HomePage() {
             <input
               type="text"
               placeholder="add a new task ..."
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
               className="bg-slate-100 py-4 px-4 rounded-md flex-1 text-xl"
             />
-            <button className=" bg-slate-100 px-4 py-2 rounded-md hover:bg-slate-500 hover:text-slate-100 ml-4">
+            <button
+              className=" bg-slate-100 px-4 py-2 rounded-md hover:bg-slate-500 hover:text-slate-100 ml-4"
+              onClick={() => addTask(inputValue)}
+            >
               Create
             </button>
           </div>
           <div className="border border-red-500 w-full h-full flex flex-col items-center justify-start">
             {isLoading && <LoadingSpinner />}
-
-            {tasks &&
+            {!isLoading &&
+              tasks &&
               tasks.length > 0 &&
               tasks.map((task: Task, idx: number) => (
                 <TaskItem task={task} key={idx} />
               ))}
-            {tasks && tasks.length === 0 && (
+
+            {!isLoading && tasks && tasks.length === 0 && (
               <p>add some tasks to get started!</p>
             )}
           </div>
--- frontend/src/services/task.ts
+++ frontend/src/services/task.ts
@@ -1,3 +1,7 @@
 import apiFetch from "./apiFetch";
 
 export const getTasks = () => apiFetch("GET", "tasks");
+export const addTask = (title: string) =>
+  apiFetch("POST", "tasks", {
+    title,
+  });
